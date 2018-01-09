a6e8c0184a6eb13daaa222bd166e54c4ea38f963
Mrunal Patel <mrunalp@gmail.com>
2018-01-09 14:53:36 -0800

    Add support for custom log directory
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate serde_json;
 extern crate nix;
 extern crate daemonize;
 
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate clap;
+#[macro_use] extern crate serde_derive;
 
 use std::io::{self, Read};
 use nix::unistd::execvp;
 use std::ffi::CString;
+use std::path::Path;
 use daemonize::Daemonize;
 
 #[derive(Serialize, Deserialize)]
@@ -18,22 +19,29 @@ struct ContainerState {
 }
 
 fn main() {
+    let matches = clap_app!(ocistracehook =>
+        (version: "0.1.0")
+        (about: "OCI strace hook")
+        (@arg LOGDIR: -l --logdir +takes_value "Specify directory for storing logs")
+    ).get_matches();
+
+    let log_dir = matches.value_of("logdir").unwrap_or("/tmp");
+
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
     let cs: ContainerState = serde_json::from_str(&buffer).unwrap();
 
     println!("ID: {}, Pid: {}", cs.id, cs.pid);
-    let pid_str = cs.pid.to_string();
+    let log_file_path = Path::new(log_dir).join(cs.id).as_os_str().to_str().unwrap().to_string();
+    println!("LogPath: {}", log_file_path);
+
     let prog = CString::new("strace").unwrap();
     let args = &[CString::new("strace").unwrap(), CString::new("-f").unwrap(),
-                            CString::new("-o").unwrap(), CString::new("/tmp/strace.out").unwrap(),
-                             CString::new("-p").unwrap(), CString::new(pid_str).unwrap()];
+                 CString::new("-o").unwrap(), CString::new(log_file_path).unwrap(),
+                 CString::new("-p").unwrap(), CString::new(cs.pid.to_string()).unwrap()];
 
-    let daemonize = Daemonize::new()
-        .pid_file("/tmp/test.pid")
-        .chown_pid_file(true)
-        .working_directory("/tmp")
-        .umask(0o777);
+    let daemonize = Daemonize::new().working_directory("/tmp").umask(0o777);
     daemonize.start().unwrap();
+
     execvp(&prog, args).unwrap();
 }
